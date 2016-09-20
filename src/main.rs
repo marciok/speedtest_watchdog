25ca3d8b9a59ed69bb1ea5d5a2e1d32dff248cb4
Marcio Klepacz <marcioklepacz@gmail.com>
2016-09-20 12:21:44 -0300

    Removed comments

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,16 @@ use std::net::TcpStream;
 use speedtest::csv::*;
 use speedtest::g_drive::file::*;
 
-
 const GOOGLE_DNS: &'static str = "8.8.8.8:53";
 const FILE: &'static str = "speedtest.csv";
 
 fn main() {
 
-    //1. Verify is connected
-    let connected = connect_with_ip(GOOGLE_DNS);
+    let connected = TcpStream::connect(GOOGLE_DNS).is_ok();
     println!("Connected: {}", connected);
+
     let created = writer::fill(FILE, connected).unwrap();
 
-    //6. Parse the result into a Connection struct(TODO)
-    //7. Otherwise create Connection with the default values(TODO)
-    //8. Push connection into the file(TODO)
-
-    //9. Create file hasn't already on Google Drive or just update 
     if connected {
         println!("Uploading file");
         let config = UploaderConfig{ 
@@ -32,9 +26,4 @@ fn main() {
         let uploader = Uploader::new(config);
         println!("Finished: {:?}", uploader.upload(FILE, created));
     }
-}
-
-
-fn connect_with_ip(ip: &str) -> bool {
-    TcpStream::connect(ip).is_ok()
 }
